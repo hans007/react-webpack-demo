b5713a308e841b3489038edb337b9753657f0ca8
七把刀 <tao.hans@gmail.com>
2017-05-06 08:23:51 +0800

    加入注释

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,17 @@ var webpack = require('webpack');
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common'); // 合并重复代码到 common.js 文件中
 var webpackHelper = require("./webpack/webpackHelper");
 
+// src目录下的js文件列表
 var dirListArray = webpackHelper.getDirlistArray(__dirname+'/src/');
+
+// 入口文件json
 var entry = webpackHelper.getEntry(dirListArray);
 //console.log(entry);
+
+// 根据模板在build下创建呈现html
 webpackHelper.createHtml(__dirname + '/src/tpl/page.html',dirListArray);
 
-
-// // 根据模板 生成html
-// // 模板文件
-// var tpHtml = fs.readFileSync(__dirname + '/src/tpl/page.html','utf-8');
-// //console.log(tpHtml);
-
-// // 读取入口文件
-// var entry = {};
-// fs.readdirSync(__dirname+'/src/').forEach(function(filename){
-//     if(path.extname(filename) != '.js'){
-//         return;
-//     }
-
-//     // 读取入口文件
-//     var basename = path.basename(filename, '.js');
-//     entry[basename] = __dirname + '/src/' + filename;
-//     console.log('入口:' + filename);
-
-//     // 创建文件-覆盖
-//     var newFileName = __dirname + '/build/' + basename + '.html';
-//     var newFileHtml = tpHtml.replace('[filename]',filename);
-//     fs.writeFileSync(newFileName,newFileHtml);
-//     console.log('新建:' + newFileName);
-// });
-// //console.log(entry);
-
+// webpack config
 module.exports = {
     entry: entry,
     devtool: 'source-map',
